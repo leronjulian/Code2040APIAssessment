779b1bc2d31ae379bfee381f71d79f1c2ee76895
leronjulian <leronjulian@hotmail.com>
2016-09-19 18:44:36 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+import json, requests
+
+r = requests.post('https://github.com/leronjulian/Code2040APIAssessment')
+
+json.dumps= ({'token': '4aaf31adb21e2baeec7768d879744674', 'github':'https://github.com/leronjulian/Code2040APIAssessment'})
