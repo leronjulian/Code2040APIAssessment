c693bb94ea7e210828887764baa79ab76381a1f7
leronjulian <leronjulian@hotmail.com>
2016-09-19 15:17:53 -0400

    Add files via upload

---
+++ code2040.py
@@ -0,0 +1,5 @@
+import requests
+import json
+requests.post(http://challenge.code2040.org/api/register,
+             json =  {'token':'ed469c3c9f95a14f27a5d1f6d6363358',
+                      'github':'https://github.com/leronjulian/Code2040APIAssessment'}
