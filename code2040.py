34ca57177af791b80e1fb49fddd78712d49c4fde
leronjulian <leronjulian@hotmail.com>
2016-09-19 15:36:31 -0400

    Add files via upload

--- code2040.py
+++ code2040.py
@@ -1,5 +1,10 @@
 import requests
 import json
-requests.post('http://challenge.code2040.org/api/register')
-             json =  {'token':'ed469c3c9f95a14f27a5d1f6d6363358',
-                      'github':'https://github.com/leronjulian/Code2040APIAssessment'}
+
+token = "ed469c3c9f95a14f27a5d1f6d6363358"
+github = "https://github.com/leronjulian/Code2040APIAssessment"
+url = "http://challenge.code2040.org/api/register"
+payload = json.dumpes({'token' : token, 'github' : github})
+
+r = requests.post(url, payload)
+token = json.loads(r.text)["result"]
