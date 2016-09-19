188ea60e447fa3576dd5c2ffff3fbeb56d54753a
leronjulian <leronjulian@hotmail.com>
2016-09-19 18:59:31 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import json, requests
 
 url = 'https://github.com/leronjulian/Code2040APIAssessment'
 token = '4aaf31adb21e2baeec7768d879744674'
-payload = {'token': token, 'url': url}
+payload = {'token': '4aaf31adb21e2baeec7768d879744674', 'url': 'https://github.com/leronjulian/Code2040APIAssessment'}
 
-response = requests.post(url, data = json.dumps(payload)}
+response = requests.post(url, data = json.dumps(payload))
