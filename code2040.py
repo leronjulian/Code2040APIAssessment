5a27bdb525dd15ebe818a2e609d2d393650d09da
leronjulian <leronjulian@hotmail.com>
2016-09-19 18:28:31 -0400

    Add files via upload

--- code2040.py
+++ code2040.py
@@ -1,32 +1,33 @@
-import requests, iso8601, json
-def complete_challenge(token):
-	github = "https://github.com/leronjulian/Code2040APIAssessment"
-	url = "http://challenge.code2040.org/api/register"
-	payload = json.dumpes({'token' : token, 'github' : github})
+import requests, json
 
-	r = requests.post(url, payload)
-	token = json.loads(r.text)["result"]
+token = "ed469c3c9f95a14f27a5d1f6d6363358"
+github = "https://github.com/leronjulian/Code2040APIAssessment"
+url = "http://challenge.code2040.org/api/register"
+payload = json.dumps({'token' : token, 'github' : github})
+
+r = requests.post(url, payload)
+token = json.loads(r.text)["result"]
 
 	# =============================================================================
 	# Stage I: Reverse a string
 	# =============================================================================
 
-	token = "http://challenge.code2040.org/api/reverse"
-	url = "http://challenge.code2040.org/api/reverse"
-	payload = json.dumps({'token': token})
+token = "http://challenge.code2040.org/api/reverse"
+url = "http://challenge.code2040.org/api/reverse"
+payload = json.dumps({'token': token})
 
-	r = requests.post(url, payload)
-	stringToReverse = json.loads(r.text)["result"]
+r = requests.post(url, payload)
+stringToReverse = json.loads(r.text)["result"]
 
-	def reverse(string):
-	    return string[::-1]
+def reverse(string):
+	return string[::-1]
 
-	string2 = reverse(stringToReverse)
+string2 = reverse(stringToReverse)
 
-	url = "http://challenge.code2040.org/api/reverse/validate"
-	payload = json.dumps({'token' : token, 'string': string2})
+url = "http://challenge.code2040.org/api/reverse/validate"
+payload = json.dumps({'token' : token, 'string': string2})
 
-	r = requests.post(url, payload)
-	result = json.loads(r.text)["result"]
-	print (result)
-complete_challenge("ed469c3c9f95a14f27a5d1f6d6363358")
+r = requests.post(url, payload)
+result = json.loads(r.text)["result"]
+print (result)
+
